14a98f5e7fb2c20aa923e335f7317b3b79542c4f
banesullivan <banesulli@gmail.com>
2022-01-21 23:57:48 -0700

    Fix sudo

--- apt.sh
+++ apt.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get update && sudo apt-get install xargs
-xargs sudo apt-get install --no-install-recommends --yes <apt.txt \
+apt-get update && apt-get install xargs
+xargs apt-get install --no-install-recommends --yes <apt.txt \
   && \
-  sudo rm -rf /var/lib/apt/lists/*
+ rm -rf /var/lib/apt/lists/*
